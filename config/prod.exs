b54a3ab8ec9c8d251f15127025165b71b199abcd
mark-kohrman-di <111455533+mark-kohrman-di@users.noreply.github.com>
2024-08-07 16:30:51 -0500

    setup production deploys

--- config/prod.exs
+++ config/prod.exs
@@ -26,6 +26,14 @@ System.get_env("OMDB_API_KEY")
 #   database: "",
 #   ssl: true,
 #   pool_size: 2
+config :film_app, FilmAppWeb.Endpoint,
+  http: [port: {:system, "PORT"}], # Possibly not needed, but doesn't hurt
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE"),
+  server: true
+
 config :film_app, FilmApp.Repo,
+  adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  ssl: true,
+  pool_size: 2 # Free tier db only allows 4 connections. Rolling deploys need pool_size*(n+1) connections where n is the number of app replicas.
