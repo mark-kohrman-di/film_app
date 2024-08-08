ea1b4ca901c5f525f3f432976effc46463154f04
mark-kohrman-di <111455533+mark-kohrman-di@users.noreply.github.com>
2024-08-08 14:32:00 -0500

    rm username etc

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,9 +34,9 @@ if config_env() == :prod do
   config :film_app, FilmApp.Repo,
     # ssl: true,
     url: database_url,
-    username: System.get_env("username"),
-    password: System.get_env("password"),
-    database: System.get_env("database"),
+    # username: System.get_env("username"),
+    # password: System.get_env("password"),
+    # database: System.get_env("database"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
     socket_options: maybe_ipv6,
     omdb_api_key: omdb_api_key
