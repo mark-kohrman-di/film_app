bb7a077f7f6014ef5cb85433b69511c0d2aeff61
mark-kohrman-di <111455533+mark-kohrman-di@users.noreply.github.com>
2024-08-13 14:57:18 -0500

    add check origin

--- config/runtime.exs
+++ config/runtime.exs
@@ -38,8 +38,9 @@ if config_env() == :prod do
     # password: System.get_env("password"),
     # database: System.get_env("database"),
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
-    # omdb_api_key: omdb_api_key
+    socket_options: maybe_ipv6,
+    check_origin: ["https://" <> System.get_env("APP_NAME") <> ".gigalixirapp.com"]
+
 
   # The secret key base is used to sign/encrypt cookies and other secrets.
   # A default value is used in config/dev.exs and config/test.exs but you
